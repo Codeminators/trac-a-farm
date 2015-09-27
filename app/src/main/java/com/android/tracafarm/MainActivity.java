93ee74e88f9eabf174f69fc22a88c45912e8ae5b
Prempal Singh <prempal.42@gmail.com>
2015-09-27 15:17:30 +0530

    main activity layout

--- app/src/main/java/com/android/tracafarm/MainActivity.java
+++ app/src/main/java/com/android/tracafarm/MainActivity.java
@@ -4,13 +4,172 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button submitButton;
+    private EditText farmAreaEdTxt;
+    private Spinner farmLocation;
+    private ProgressBar progressBar;
+
+    public static final String[] districts = {
+            "AGRA",
+            "AHMEDABAD",
+            "AHMEDNAGAR",
+            "AKOLA",
+            "ALIGARH",
+            "ALLAHABAD",
+            "AMBEDKAR NGR.",
+            "AMRAVATI",
+            "AMRELI",
+            "ANAND",
+            "AURAIYA",
+            "AURANGABAD",
+            "AZAMGARH",
+            "BADAUN",
+            "BAGPAT",
+            "BAHRAICH",
+            "BALLIA",
+            "BALRAMPUR",
+            "BANAS KANTHA",
+            "BANDA",
+            "BARABANKI",
+            "BAREILLY",
+            "BASTAR",
+            "BASTI",
+            "BEED",
+            "BHAVNAGAR",
+            "BIJNOR",
+            "BILASPUR",
+            "BROACH",
+            "BULDHANA",
+            "BULLANDSHAHR",
+            "CHANDAULI",
+            "CHANDRAPUR",
+            "CHITRAKUT",
+            "DANGS",
+            "DANTEWARA",
+            "DEORIA",
+            "DHAMTARI",
+            "DHULE",
+            "DOHAD",
+            "DURG",
+            "ETAH",
+            "ETAWAH",
+            "FAIZABAD",
+            "FARRUKHABAD",
+            "FATEHPUR",
+            "FIROZABAD",
+            "G.BUDDHA NGR.",
+            "GADCHIROLI",
+            "GANDHINAGAR",
+            "GHAZIABAD",
+            "GHAZIPUR",
+            "GONDA",
+            "GORAKHPUR",
+            "HAMIRPUR",
+            "HARDOI",
+            "HINGOLI",
+            "J.B.PHULE NGR.",
+            "JALANA",
+            "JALAUN",
+            "JALGAON",
+            "JAMNAGAR",
+            "JANJGIR-CHAMPA",
+            "JASHPUR",
+            "JAUNPUR",
+            "JHANSI",
+            "JUNAGARH",
+            "KANKER",
+            "KANNAUJ",
+            "KANPUR CITY",
+            "KAUSHAMBI",
+            "KAWARDHA (KABIRDHAM)",
+            "KHEDA",
+            "KHERI",
+            "KOLHAPUR",
+            "KORBA",
+            "KORIYA",
+            "KUSHI NGR.",
+            "KUTCH",
+            "LALITPUR",
+            "LATUR",
+            "LUCKNOW",
+            "MAHAMAYA NAGAR",
+            "MAHARAHGANJ",
+            "MAHASMUND",
+            "MAHOBA",
+            "MAINPURI",
+            "MANDURBAR",
+            "MATHURA",
+            "MAU",
+            "MEERUT",
+            "MEHSANA",
+            "MIRZAPUR",
+            "MIXED CROPS",
+            "MORADABAD",
+            "MUZAFFARNAGAR",
+            "NAGPUR",
+            "NANDED",
+            "NARMADA",
+            "NASIK",
+            "NAVSARI",
+            "OSMANABAD",
+            "PANCH MAHALS",
+            "PARBHANI",
+            "PATAN",
+            "PILIBHIT",
+            "PORBANDAR",
+            "PRATAPGARH",
+            "PUNE",
+            "RAEBARELI",
+            "RAIGARH",
+            "RAIPUR",
+            "RAJ NANDGAON",
+            "RAJKOT",
+            "RAMABAI NAGAR",
+            "RAMPUR",
+            "S.RAVI DAS NGR.",
+            "SABARKANTHA",
+            "SAHARANPUR",
+            "SANGLI",
+            "SANT KABIR NGR.",
+            "SARGUJA",
+            "SATARA",
+            "SHAHJAHANPUR",
+            "SHIVASTI",
+            "SIDDHARTH NGR.",
+            "SITAPUR",
+            "SOLAPUR",
+            "SONBHADRA",
+            "SULTANPUR",
+            "SURAT",
+            "SURENDRANAGAR",
+            "TAPI",
+            "UNNAO",
+            "VADODARA",
+            "VALSAD",
+            "VARANASI",
+            "WARDHA",
+            "WASHIM",
+            "YAVATMAL"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton = (Button) findViewById(R.id.submit);
+        farmAreaEdTxt = (EditText) findViewById(R.id.farm_area);
+        farmLocation = (Spinner) findViewById(R.id.farm_location);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, districts);
+        farmLocation.setAdapter(dataAdapter);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
     }
 
     @Override
